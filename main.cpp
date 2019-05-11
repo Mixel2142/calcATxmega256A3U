3a14657944d12ef6d0ef891406dcd08a45cbb26f
Mixel2142 <mihefremov@sfedu.ru>
2019-05-11 12:52:00 +0300

    last2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 //atxmega256A3U
+#define F_CPU 32000000
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -8,6 +9,7 @@
 #define Did  2
 #define Mul  3
 #define Equ  4
+#define Nun 0xff
 #define i 0b11111011
 #define n 0b10101011
 #define f 0b10001110
@@ -26,13 +28,13 @@
 #define Point 0b01111111
 
 // кол-во итераций в ISR которое нужно для срабатывания нажатия клавиши (работает совместно с keyCounter)
-const uint8_t keyDelay = 70;   
+const uint8_t keyDelay = 70;
 
 // 1/195 => 0.0051 сек * 40к = 205 сек (работает совместно с powerCounter)
-const uint16_t powerStop = 40000;	
+const uint16_t powerStop = 40000;
 
 // задержка на индикацию нажатого действия (* / - + =)
-const uint8_t delaySign = 50;	
+const uint8_t delaySign = 50;
 
 const uint8_t numb[10] = {
 	0b11000000, // 0
@@ -40,74 +42,65 @@ const uint8_t numb[10] = {
 	0b10100100, // 2
 	0b10110000, // 3
 	0b10011001, // 4
-	0b10010010, // 5 
+	0b10010010, // 5
 	0b10000010, // 6
 	0b11011000, // 7
 	0b10000000, // 8
 	0b10010000  // 9
 };
-	
-// разрешает обработку кнопки
-bool isCanPressKey = true;
 
 // показывает событие нажатия кнопки
-bool pressKey = false; 
+bool pressKey = false;
 
 //писать в переменную numberOne
-bool writeTo = NumOne; 
+bool writeTo = NumOne;
 bool inf = false;
 
-// счётчик кол-ва нажатия в ISR
-uint8_t keyCounter = 0;
-
-// счётчик для переключения режима питания МК  
-uint16_t powerCounter = 0;
-	
 // хранит в себе данные для вывода на дисплей
-uint8_t dispVal[4] = {255,255,255,numb[0]}; 
+uint8_t dispVal[4] = {Nun,Nun,Nun,numb[0]};
 
 // переменные для хранения/записи/расчетов вычислений
-float numberOne_f = 0.0;
+float numberOne = 0.0;
 float numberTwo = 0.0;
 
-// кодирует 10-е число в 2-е
-uint8_t coder(uint8_t code)
+void Plus_Sign()
 {
-	code -=1;
-	code = code -((code >> 1) & 0b01010101);
-	return (code & 0b00110011) + ((code >> 2) & 0b00110011);
+	numberOne += numberTwo;
 }
 
-void DrawZnakomesto()
+void Sub_Sign()
 {
-	PORTD.DIR = PORTD.DIR << 1;
-	if(PORTD.DIR > 0b00001000)
+	numberOne -= numberTwo;
+}
+
+void Did_Sign()
+{
+	if(numberTwo == 0.0)
 	{
-	PORTD.DIR = 0b00000001;
+		inf = true;
+		numberOne = 0.0;
+		numberTwo = 0.0;
 	}
-	PORTE.OUT = PORTD.DIR;
-	PORTC_OUT = dispVal[coder(PORTD.DIR)];
+	else
+	{
+		numberOne /= numberTwo;
+	}
 }
 
-bool isKeyPres()
+void Mul_Sign()
 {
-	return (PORTE.IN > 0b00001111);
+	numberOne *= numberTwo;
 }
 
-bool isKeyPres(uint8_t key)
+// Нужен для того, чтобы не вышли за пределы массива  masSign
+void Equ_Sign()
 {
-	return (PORTE.IN == key);
 }
 
-uint8_t getColumn()
-{
-	return coder(PORTE.IN & 0b00001111);
-}
 
-uint8_t getRow()
-{
-	return coder((PORTE.IN & 0b11110000) >> 4);
-}
+typedef void ( *VoidFunPtrSign )();
+
+const VoidFunPtrSign masSign[5] = {Plus_Sign, Sub_Sign, Did_Sign, Mul_Sign, Equ_Sign};
 
 void key0(uint8_t* sign,float* number)
 {
@@ -165,20 +158,20 @@ void On_C_Board(uint8_t* sign,float* number)
 	{
 		inf	= false;
 		numberTwo = 0.0;
-		numberOne_f = 0.0;
+		numberOne = 0.0;
 		writeTo = NumOne;
 		(*sign) = 0;
 	}
 	
-	int32_t num_i= *number;
-	
-	if(num_i == 0)
+	if(*number == 0.0)
 	{
 		writeTo = NumOne;
-	}	
-	
-	num_i /= 10;
-	*number = num_i;
+	}
+	else
+	{
+		*number -= static_cast< int32_t >( *number ) % 10;
+		*number /= 10;
+	}
 }
 
 void Plus_Board(uint8_t* sign,float* number)
@@ -196,7 +189,7 @@ void Sub_Board(uint8_t* sign,float* number)
 {
 	writeTo = NumTwo;
 	*sign = Sub;
-	dispVal[0] = 255;
+	dispVal[0] = Nun;
 	dispVal[1] = s;
 	dispVal[2] = u;
 	dispVal[3] = b;
@@ -207,7 +200,7 @@ void Did_Board(uint8_t* sign,float* number)
 {
 	writeTo = NumTwo;
 	*sign = Did;
-	dispVal[0] = 255;
+	dispVal[0] = Nun;
 	dispVal[1] = d;
 	dispVal[2] = i;
 	dispVal[3] = d;
@@ -229,7 +222,7 @@ void Equ_Board(uint8_t* sign,float* number)
 {
 	writeTo = NumOne;
 	
-	dispVal[0] = 255;
+	dispVal[0] = Nun;
 	dispVal[1] = e;
 	dispVal[2] = q;
 	dispVal[3] = u;
@@ -249,47 +242,7 @@ const VoidFunPtrBoard keyBoard[4][4] = {
 	{Mul_Board,key6,key5,key4},
 	{Sub_Board,key3,key2,key1},
 	{Plus_Board,Equ_Board,key0,On_C_Board}};
-	
 
-
-void Plus_Sign()
-{
-	numberOne_f += numberTwo;
-}
-
-void Sub_Sign()
-{
-	numberOne_f -= numberTwo;
-}
-
-void Did_Sign()
-{
-	if(numberTwo == 0.0)
-		{
-			inf = true;
-			numberOne_f = 0.0;
-			numberTwo = 0.0;
-		}
-		else
-		{
-			numberOne_f /= numberTwo;
-		}
-}
-
-void Mul_Sign()
-{
-	numberOne_f *= numberTwo;
-}
-
-void Equ_Sign()
-{
-	
-}
-
-typedef void ( *VoidFunPtrSign )(uint8_t* ,float* );
-
-const VoidFunPtrSign masSign[5] = {Plus_Sign, Sub_Sign, Did_Sign, Mul_Sign, Equ_Sign};
-	
 uint8_t makeDispVal(uint8_t num)
 {
 	return( numb[ num ] );
@@ -332,16 +285,21 @@ int32_t pow_dec(uint8_t pw)
 
 // заполняет массив dispVal[]
 void makeDisplayValue()
-{	
-	int32_t numberOne_i = numberOne_f;
-	uint16_t digitsAfterPoint = mod((numberOne_f - numberOne_i) * 1000);
-	
+{
+	dispVal[0] = Nun;
+	dispVal[1] = Nun;
+	dispVal[2] = Nun;
+	dispVal[3] = numb[0];
+
 	if(writeTo == NumOne)
 	{
+		int32_t numberOne_i = numberOne;
+		uint16_t digitsAfterPoint = mod((numberOne - numberOne_i) * 1000);
+
 		// при делении на 0
 		if(inf)
 		{
-			dispVal[0] = 255;
+			dispVal[0] = Nun;
 			dispVal[1] = i;
 			dispVal[2] = n;
 			dispVal[3] = f;
@@ -350,7 +308,7 @@ void makeDisplayValue()
 		{
 			uint8_t lenNumberOne = getLength(numberOne_i);
 			
-			if(numberOne_f < 0)
+			if(numberOne < 0)
 			{
 				if(lenNumberOne < 4)
 				{
@@ -415,11 +373,11 @@ void makeDisplayValue()
 					}
 				}
 				else
-				{	
+				{
 					dispVal[3] = makeDispVal(lenNumberOne-1);
 					dispVal[2] = e;
 					dispVal[1] = makeDispVal(numberOne_i/pow_dec(lenNumberOne-1)%10);
-					dispVal[0] = 255;
+					dispVal[0] = Nun;
 				}
 			}
 		}
@@ -437,102 +395,164 @@ void makeDisplayValue()
 	}
 }
 
+bool isKeyPres()
+{
+	return (PORTE.IN > 0b00001111);
+}
+
+// кодирует 10-е число в 2-е
+uint8_t coder(uint8_t code)
+{
+	code -=1;
+	code = code -((code >> 1) & 0b01010101);
+	return (code & 0b00110011) + ((code >> 2) & 0b00110011);
+}
+
+void DrawZnakomesto()
+{
+	PORTD.DIR = PORTD.DIR << 1;
+	if(PORTD.DIR > 0b00001000)
+	{
+		PORTD.DIR = 0b00000001;
+	}
+	PORTE.OUT = PORTD.DIR;
+	PORTC_OUT = dispVal[coder(PORTD.DIR)];
+}
+
+
+
 // прерывания по переполнению таймера ТСС0
 ISR(TCC0_OVF_vect)
 {
-		DrawZnakomesto();
+	// разрешает обработку кнопки
+	static bool isCanPressKey = true;
 
-		keyCounter++;
-		if(keyCounter > keyDelay)
+	// счётчик кол-ва нажатия в ISR
+	static uint8_t keyCounter = 0;
+
+	// счётчик для переключения режима питания МК
+	static uint16_t powerCounter = 0;
+
+	DrawZnakomesto();
+
+	keyCounter++;
+	if(keyCounter > keyDelay)
+	{
+		isCanPressKey = true;
+		keyCounter = 0;
+	}
+	
+	if(isKeyPres())
+	{
+		if(isCanPressKey)
 		{
-			isCanPressKey = true;
+			isCanPressKey = false;
+			pressKey = true;
 			keyCounter = 0;
 		}
-			
-		if(isKeyPres())
+		
+		powerCounter = 0;
+	}
+	else
+	{
+		powerCounter++;
+		if(powerCounter > powerStop)
 		{
-			if(isCanPressKey)
-			{
-				isCanPressKey = false;
-				pressKey = true;
-				keyCounter = 0;
-			}
-	
 			powerCounter = 0;
+			keyCounter = 0;
+			//power = false; включить мод сна
 		}
-		else
-		{
-			powerCounter++;
-			if(powerCounter > powerStop)
-			{
-				powerCounter = 0;
-				keyCounter = 0;
-				//power = false; включить мод сна
-			}
-		}
+	}
 }
 
-//работает по умолчанию от генератора 2 MHz 
-void init_timer() 
+//работает по умолчанию от генератора 2 MHz
+void init_timer()
 {
 	cli();
 
 	TCC0.PER = 40;
-	TCC0.CTRLA |= TC_CLKSEL_DIV256_gc; 
+	TCC0.CTRLA |= TC_CLKSEL_DIV256_gc;
 	// 2 MHz / 255 / 40 ~= 195 Hz
 	
 	// установили низкий уровень прерывания
 	TCC0.INTCTRLA |=  TC_OVFINTLVL_LO_gc;
 	
-	// Разрешили низкий уровень прерывания	
-	PMIC.CTRL |=  PMIC_LOLVLEN_bm; 
+	// Разрешили низкий уровень прерывания
+	PMIC.CTRL |=  PMIC_LOLVLEN_bm;
 	
 	sei();
 }
 
+uint8_t getColumn()
+{
+	return coder(PORTE.IN & 0b00001111);
+}
+
+uint8_t getRow()
+{
+	return coder((PORTE.IN & 0b11110000) >> 4);
+}
+
 int main(void)
 {
-	PORTD.DIR = 0b00000001; 
-	PORTD.OUT = 0b00000000;	
+	// определяет какое знакоместо будет подсвечиваться
+	PORTD.DIR = 0b00000001;
+	PORTD.OUT = 0b00000000;
 	
-	PORTE.DIR = 0b00001111; 
-	PORTE.OUT = 0b00000001;	
+	//  работа с клавиатурой
+	PORTE.DIR = 0b00001111;
+	PORTE.OUT = 0b00000001;
 	
 	PORTE.PIN4CTRL = PORT_OPC_PULLDOWN_gc;
 	PORTE.PIN5CTRL = PORT_OPC_PULLDOWN_gc;
 	PORTE.PIN6CTRL = PORT_OPC_PULLDOWN_gc;
 	PORTE.PIN7CTRL = PORT_OPC_PULLDOWN_gc;
 
-    PORTC.DIR = 0b11111111; 
+	// управляет сегментами
+	PORTC.DIR = 0b11111111;
 	
 	init_timer();
 
 	// запоминает нужное действие (+-=:*)
-	uint8_t sign = 0; 
+	uint8_t sign = 0;
 
-	while (1) 
-    {
+	while (1)
+	{
 		if(pressKey)
 		{
 			uint8_t col = getColumn();
 			uint8_t row = getRow();
 			
-			// отсеяли помехи  
-			if(row < 4 && col < 4) 
+			// отсеяли помехи
+			if(row < 4 && col < 4)
 			{
 				if(writeTo == NumOne)
 				{
-					keyBoard[col][row]( &sign, &numberOne_f);
+					keyBoard[col][row]( &sign, &numberOne);
 				}
 				else
 				{
 					keyBoard[col][row]( &sign, &numberTwo);
 				}
 				
+				if(numberOne > 9999.0)
+				{
+					numberOne -= static_cast< int32_t >( numberOne ) % 10;
+					numberOne /= 10;
+				}
+				
+				if(numberTwo > 9999.0)
+				{
+					numberTwo -= static_cast< int32_t >( numberTwo ) % 10;
+					numberTwo /= 10;
+				}
+			
 				makeDisplayValue();
 			}
 			pressKey = false;
 		}//if pressKey
 	}
 }
+
+
 
